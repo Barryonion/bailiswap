0babf30c3552531a8b361716ae9da337c6172b8e
bailiyinfeng <zhangxucheng991@163.com>
2022-06-20 21:07:33 +0800

    update ROUTER ADDRESS

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 
-export const ROUTER_ADDRESS = '0xF6475492f33b5C8031d2351310c1d3a029025A68'
+export const ROUTER_ADDRESS = '0x3C0d93E05501a6CB4d1535a76D70139F73479e1B'
 
 // a list of tokens by chain
 type ChainTokenList = {
